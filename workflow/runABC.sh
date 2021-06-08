fdc59b4f7b7b3bf3f6c4a91cecfae181fda85211
Maya Sheth <shethm@sh01-ln01.stanford.edu>
2021-06-07 17:18:37 -0700

    initial commit

---
+++ workflow/runABC.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CODEDIR=/oak/stanford/groups/engreitz/Users/sheth/ABC-Enhancer-Gene-Prediction
+
+snakemake -s $CODEDIR/workflow/Snakefile_working --configfile $CODEDIR/config/config.yaml --directory $CODEDIR/workflow --unlock 
+
+snakemake -s $CODEDIR/workflow/Snakefile_working --configfile $CODEDIR/config/config.yaml --use-conda -j1 --conda-frontend conda --keep-target-files --rerun-incomplete
+
+
